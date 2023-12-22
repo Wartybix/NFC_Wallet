141534576d8f10117593600f437e957131fb7461
L Turnbull <lt21aai@herts.ac.uk>
2023-12-22 18:12:26 +0000

    Started adding functionality to change read/write status & added light/dark UI previews.

--- app/src/main/java/com/example/nfcwallet/MainActivity.kt
+++ app/src/main/java/com/example/nfcwallet/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.nfcwallet
 
+import android.app.PendingIntent
+import android.content.IntentFilter
+import android.content.res.Configuration
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +15,26 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nfcwallet.ui.theme.NFCWalletTheme
 
 class MainActivity : ComponentActivity() {
+    //Using tutorial
+
+    lateinit var nfcAdapter: NfcAdapter
+    var writeMode: Boolean = false
+    var myTag: Tag? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
         setContent {
             NFCWalletTheme {
                 // A surface container using the 'background' color from the theme
@@ -34,30 +52,47 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Menu() {
     Column {
-        Button(onClick = { /*TODO*/ }) {
+        var writeMode by remember { mutableStateOf(false) }
+
+        Button(onClick = {
+            writeMode = false
+        }) {
             Text(text = "Read")
         }
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            writeMode = true
+        }) {
             Text(text = "Project")
         }
 
-        Text(text = "Hello world.")
+
+        Text(
+            text = if (writeMode) "Write Mode" else "Read Mode",
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Text(
+            text = "Tag data here",
+            style = MaterialTheme.typography.bodyMedium
+        )
     }
 }
 
+@Preview(
+    name = "Light Mode",
+    showBackground = true
+)
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun Preview() {
     NFCWalletTheme {
-        Menu()
+        Surface {
+            Menu()
+        }
     }
 }
