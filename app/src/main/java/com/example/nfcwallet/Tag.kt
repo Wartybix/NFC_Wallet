3452961c16c685b1018e38131eb5263b506de95f
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-03-25 03:45:23 +0000

    Implemented read/write IO operations.

--- app/src/main/java/com/example/nfcwallet/Tag.kt
+++ app/src/main/java/com/example/nfcwallet/Tag.kt
@@ -1,8 +1,9 @@
 package com.example.nfcwallet
 
 import androidx.compose.ui.graphics.ImageBitmap
+import java.io.Serializable
 
 class Tag(
     var name: String,
     var image: ImageBitmap?,
-)
+) : Serializable
--- app/src/main/java/com/example/nfcwallet/ui/WalletViewModel.kt
+++ app/src/main/java/com/example/nfcwallet/ui/WalletViewModel.kt
@@ -1,30 +1,68 @@
 package com.example.nfcwallet.ui
 
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import com.example.nfcwallet.Tag
 import com.example.nfcwallet.data.WalletUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
-private fun getExampleData() = listOf(
-    null to "Pigeon Card",
-    null to "Passport",
-    null to "Shopping Card",
-    null to "Other Card",
-    null to "Cardigan Card",
-    null to "Student Card",
-    null to "Staff Card",
-    null to "Cardboard card"
-).map { Tag(it.second, it.first) }
+private const val USER_STORAGE_FILE_PATH = "tag_data"
 
-class WalletViewModel : ViewModel() {
+private fun loadTagsFromStorage(context: Context) : SnapshotStateList<Tag> {
+    var tags: SnapshotStateList<Tag>
+    try {
+        val fis = context.openFileInput(USER_STORAGE_FILE_PATH)
+        val ois = ObjectInputStream(fis)
+
+        tags = (ois.readObject() as List<Tag>).toMutableStateList()
+
+        ois.close()
+        fis.close()
+    } catch (e: Exception) { // This is normal, e.g. if the user has opened the app for the 1st time
+        e.printStackTrace()
+        tags = SnapshotStateList()
+    }
+    Log.d("Loading tags", "Loading tags right now")
+
+    return tags
+}
+
+private fun saveTagsToStorage(context: Context, tags: SnapshotStateList<Tag>) {
+    Log.d("Saving tag", tags.toString())
+
+    val serializableTags = tags.toList<Tag>()
+
+    try {
+        val fos: FileOutputStream = context.openFileOutput(USER_STORAGE_FILE_PATH, Context.MODE_PRIVATE)
+        val oos = ObjectOutputStream(fos)
+
+        oos.writeObject(serializableTags)
+
+        oos.close()
+        fos.close()
+
+        Log.d("Tag saved", "successful")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+class WalletViewModel(application: Application) : AndroidViewModel(application) {
     private val _uiState = MutableStateFlow(WalletUiState())
+
     val uiState: StateFlow<WalletUiState> = _uiState.asStateFlow()
-    val _tags = getExampleData().toMutableStateList()
+    val _tags = loadTagsFromStorage(getApplication<Application>().applicationContext)
+
 
     fun enableReceiver() {
         _uiState.update { currentState ->
@@ -45,9 +83,11 @@ class WalletViewModel : ViewModel() {
 
     fun addTag(tag: Tag) {
         _tags.add(tag)
+        saveTagsToStorage(getApplication<Application>().applicationContext, _tags)
     }
 
     fun removeTag(tag: Tag) {
         _tags.remove(tag)
+        saveTagsToStorage(getApplication<Application>().applicationContext, _tags)
     }
 }
