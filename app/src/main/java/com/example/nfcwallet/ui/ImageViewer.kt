22be290eb93794b5a39d10c31555dd66f11b0e3e
wartybix <34974060+Wartybix@users.noreply.github.com>
2024-04-01 16:39:55 +0100

    Added zoom/pan functionality to the image viewer.

--- app/src/main/java/com/example/nfcwallet/ui/ImageViewer.kt
+++ app/src/main/java/com/example/nfcwallet/ui/ImageViewer.kt
@@ -1,10 +1,19 @@
 package com.example.nfcwallet.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nfcwallet.R
@@ -15,10 +24,25 @@ fun ImageViewer(
     image: ImageBitmap,
     modifier: Modifier = Modifier
 ) {
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val transformableState = rememberTransformableState { zoomChange, offsetChange, _ ->
+        scale *= zoomChange
+        offset += offsetChange
+    }
+
     Image(
         bitmap = image,
         contentDescription = null,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                translationX = offset.x,
+                translationY = offset.y
+            )
+            .transformable(state = transformableState)
+            .fillMaxSize()
     )
 }
 
