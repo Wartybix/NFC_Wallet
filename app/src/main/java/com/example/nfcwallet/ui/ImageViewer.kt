5209658b547d259045b76713c7d8fb2f5d13c2fb
Wartybix <34974060+Wartybix@users.noreply.github.com>
2024-04-02 13:44:06 +0100

    Now using usuiat's Zoomable library to zoom the image in ImageViewer. Easier to implement the minimum zoom boundary this way, with double-tap gestures.

--- app/src/main/java/com/example/nfcwallet/ui/ImageViewer.kt
+++ app/src/main/java/com/example/nfcwallet/ui/ImageViewer.kt
@@ -1,48 +1,30 @@
 package com.example.nfcwallet.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nfcwallet.R
 import com.example.nfcwallet.ui.theme.NFCWalletTheme
+import net.engawapg.lib.zoomable.rememberZoomState
+import net.engawapg.lib.zoomable.zoomable
 
 @Composable
 fun ImageViewer(
     image: ImageBitmap,
     modifier: Modifier = Modifier
 ) {
-    var scale by remember { mutableFloatStateOf(1f) }
-    var offset by remember { mutableStateOf(Offset.Zero) }
-    val transformableState = rememberTransformableState { zoomChange, offsetChange, _ ->
-        scale *= zoomChange
-        offset += offsetChange
-    }
+    val zoomState = rememberZoomState()
 
     Image(
         bitmap = image,
         contentDescription = null,
         modifier = modifier
-            .graphicsLayer(
-                scaleX = scale,
-                scaleY = scale,
-                translationX = offset.x,
-                translationY = offset.y
-            )
-            .transformable(state = transformableState)
             .fillMaxSize()
+            .zoomable(zoomState)
     )
 }
 
